52ff05fa1a6b93e445080076ca2994bbbcca7077
Ramon Vicente <ramonvic@me.com>
2019-11-06 09:13:05 -0300

    Fix typo

--- src/Exception/BadResponseException.php
+++ src/Exception/BadResponseException.php
@@ -9,7 +9,7 @@ class BadResponseException extends \RuntimeException
 
     public function __construct($message, $response = null, $responseString = null) {
         $code = preg_replace("/\D+/", "", $response->ListaMensagemRetorno->MensagemRetorno->Codigo ?? "") ?? -1;
-        parent::__construct($message, $code, $previous);
+        parent::__construct($message, $code, null);
         $this->response = $response;
         $this->responseString = $responseString;
     }
